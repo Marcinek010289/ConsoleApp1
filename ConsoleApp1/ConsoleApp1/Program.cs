7eb7b370447a302bf33d1c80971185c047b4b5c7
Marcin W <marcin890201@o2.pl>
2023-06-06 19:09:07 +0200

    zadanie 4 dzień

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool woman = true;
+int age = 33;
+
+if (woman == true)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
